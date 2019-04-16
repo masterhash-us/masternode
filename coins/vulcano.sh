7109b585cb1af66dc7e6a1b1c3bf4841fcd826bb
handscooped <48146965+handscooped@users.noreply.github.com>
2019-04-15 20:18:09 -0700

    Create vulcano.sh

---
+++ coins/vulcano.sh
@@ -0,0 +1,8 @@
+export TITLE="Vulcano Masternode"
+export URL="https://github.com/VulcanoCrypto/Vulcano/releases/download/2.1.0.0/Vulcano-2.1.0.0-x86_64-linux64-gnu.tar.gz"
+export CHAINURL="https://chainseeder.masterhash.us/vulcchain.tar.gz"
+export DAEMONCOMMAND="vulcanod"
+export CLICOMMAND="vulcano-cli"
+export FILES="blocks database chainstate peers.dat"
+export COINDIR=".vulcanocore"
+export CONFFILE="vulcano.conf"
