619411d4eabeae243b756dd3d665b20b2b8b09a5
handscooped <48146965+handscooped@users.noreply.github.com>
2019-04-19 19:37:57 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+echo 'vm.swappiness=10'  | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+
 clear
 cat << "EOF"
  __  __           _            _    _           _     
